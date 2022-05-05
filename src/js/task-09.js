bb2c7a71b0d3cf856022cc4346e2fbc7b273d901
Vasiliy <mazurboss2016@gmail.com>
2022-05-06 00:21:20 +0300

    update

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,14 +1,12 @@
-
 function handleChangeBackgroundColor() {
   document.body.style.backgroundColor = getRandomHexColor();
   
-  
-  function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
  };
 const buttonEl = document.querySelector('button.change-color'); 
 buttonEl.addEventListener('click', handleChangeBackgroundColor);
 const spanEl = document.querySelector('.color');
-
-// console.dir(buttonEl);
